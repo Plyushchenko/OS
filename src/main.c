6cad3e286cccbf935b84a7ae140f35261f291e4f
Plyushchenko <plyushenko@bk.ru>
2016-09-30 00:25:32 +0300

    fixes for subtask2

--- src/main.c
+++ src/main.c
@@ -18,7 +18,10 @@ void main(void)
 	init_serial_port();
 	init_idt();				
 	init_pic();	
-	init_pit();
+//	init_pit();
+	
+//	out8(MASTER_PIC_DATA_PORT, 254); //binary(11111110), enable interruptions master
+//	out8(SLAVE_PIC_DATA_PORT, 255);
 	enable_ints(); 
 	__asm__("int $3");
 
--- src/pit.c
+++ src/pit.c
@@ -3,9 +3,6 @@
 void init_pit()
 {
 	out8(PIT_COMMAND_PORT, (1 << 5) | (1 << 4) | (1 << 2)); // 2 bytes for coef (4th and 5th); type 2
-	out8(PIT_DATA_PORT, 0xae);// 1193182 = 41 * 29102; 29102 = 0xae 
+	out8(PIT_DATA_PORT, 0xae);// 1193182 = 41 * 29102; 29102 = 0x71ae 
 	out8(PIT_DATA_PORT, 0x71); 
-
-	out8(MASTER_PIC_DATA_PORT, 254); //binary(11111110), enable interruptions master
-
 }
